248a0e4262cbc688aae7689b6af7ab08ad9f3cbc
Gamzat <mukailov.g@gmail.com>
2020-02-03 22:54:22 +0100

    erste Tests

---
+++ src/components/TestUsers.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="user">
+    <div v-bind:key="user.id" v-for="user in users">
+      <User v-bind:user="user" />
+    </div>
+  </div>
+</template>
+
+<script>
+import User from "./User.vue";
+export default {
+  name: "TestUsers",
+  components: {
+    User
+  },
+  props: ["users"]
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/User.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="user">
+    <h3>{{ user.name }}</h3>
+    <h3>{{ user.email }}</h3>
+    <h3>{{ user.adresse }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User",
+  props: ["user"]
+};
+</script>
+
+<style scoped></style>
